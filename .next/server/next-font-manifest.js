a66da8ebd5c8a9c7dd8069c3ce81ba5e815c0a5e
xuhaoying <xvhaoying@outlook.com>
2025-06-09 13:23:32 +0800

    node modules

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/xuhaoying/Desktop/100-Project/AI_Web/A004_AITools/bytehive.site/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
